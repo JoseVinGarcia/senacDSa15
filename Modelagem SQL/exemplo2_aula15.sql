e1429f5b51f6876a861622a68475c7501c50f745
JoseVinGarcia <josevingarcia2020@gmail.com>
2024-10-31 21:39:57 -0300

    Conteudo Final

--- Modelagem SQL/exemplo2_aula15.sql
+++ Modelagem SQL/exemplo2_aula15.sql
@@ -1,4 +1,4 @@
-ALTER TABLE tb_pedidos
+/* ALTER TABLE tb_pedidos
 ADD CONSTRAINT fk_cliente
 FOREIGN KEY (cliente_codigo) REFERENCES tb_clientes(codigo_cliente);
 
@@ -8,4 +8,19 @@ FOREIGN KEY (item_codigo) REFERENCES tb_produtos(codigo_produto);
 
 ALTER TABLE tb_itens
 ADD CONSTRAINT fk_pedido
-FOREIGN KEY (pedido_codigo) REFERENCES tb_pedidos(codigo_pedido);
+FOREIGN KEY (pedido_codigo) REFERENCES tb_pedidos(codigo_pedido); */
+
+/*SELECT * FROM tb_pedidos WHERE valor > 50.00;
+SELECT * FROM tb_pedidos WHERE valor > 60 AND data_pedido < "2024-01-01";*/
+
+/* INNER JOIN */
+/*SELECT *
+FROM tb_pedidos
+INNER JOIN tb_clientes ON cliente_codigo=codigo_cliente;*/
+
+/* CASO REAL 1*/
+SELECT nome, CPF, email, telefone, codigo_pedido
+FROM tb_pedidos
+INNER JOIN tb_clientes
+ON cliente_codigo = codigo_cliente
+WHERE codigo_pedido = 17
